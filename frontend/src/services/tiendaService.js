22b780eb9d276b8f9324d23ec934856a2c19861d
jpabloav <jpabloav127@gmail.com>
2025-07-09 23:48:43 -0500

    Update tiendaService.js

--- frontend/src/services/tiendaService.js
+++ frontend/src/services/tiendaService.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 // Funciones para productos
 export const CrearProducto = async (id,nombre, stock, precio) => {
